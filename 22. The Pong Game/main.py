5b99e4e203b3c6fb9a624609268123ac71c7e8c3
Nur Fatin <87479168+Nur-Fatin@users.noreply.github.com>
2022-03-16 11:47:00 +0800

    Add files via upload

---
+++ 22. The Pong Game/main.py
@@ -0,0 +1,65 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("The Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=r_paddle.go_up)
+screen.onkey(key="Down", fun=r_paddle.go_down)
+screen.onkey(key="w", fun=l_paddle.go_up)
+screen.onkey(key="s", fun=l_paddle.go_down)
+
+game_on = True
+while game_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    # Detect collision with the paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+    # Detect when R paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect when L paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
