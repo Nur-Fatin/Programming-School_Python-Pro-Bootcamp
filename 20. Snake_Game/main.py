e57b9f02f25a576325452ec8a09598051a15e764
Nur Fatin <87479168+Nur-Fatin@users.noreply.github.com>
2022-04-25 10:02:30 +0800

    Add files via upload

---
+++ 20. Snake_Game/main.py
@@ -0,0 +1,52 @@
+# create snake body- 3 squares line up next to each other
+# move the snake forward;
+# control the snake with keystrokes-to change direction- up down left right arrows keys
+# create food and detect collision with the food
+# create score board
+# detect collision with wall
+# detect collision with tail
+
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
